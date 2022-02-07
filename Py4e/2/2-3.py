36a19c4b0f02fb3bccbddab0e8b3f493258b60ef
fattahsamit <fattahsamit@gmail.com>
2022-02-07 16:40:51 +0600

    Compute Gross Pay

---
+++ Py4e/2/2-3.py
@@ -0,0 +1,7 @@
+# 2.3 
+# Write a program to prompt the user for hours and 
+# rate per hour using input to compute gross pay.
+
+hours = float(input("Enter Hours:"))
+rate = float(input("Enter Rate per hour:"))
+pay = print("Pay:",hours*rate)

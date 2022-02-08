b876d06c317eeba779fa585d51525a089e9f577f
fattahsamit <fattahsamit@gmail.com>
2022-02-08 15:07:09 +0600

    5

---
+++ Py4e/5/5-2.py
@@ -0,0 +1,29 @@
+#5.2
+
+largest = None
+smallest = None
+
+while True:
+    num = input("Enter a number: ")
+    
+    if (num == "done"):
+        break
+
+    try:
+        num = int(num)
+    except:
+        print("Invalid input")
+        continue
+
+    if smallest is None:
+        smallest = num
+        largest = num
+
+    if(num > largest):
+        largest = num
+    
+    if(num < smallest):
+        smallest = num
+
+print("Maximum is", largest)
+print("Minimum is", smallest)

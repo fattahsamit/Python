45aa3c75c2044285d724cafb123959d46de68c49
fattahsamit <fattahsamit@gmail.com>
2022-05-31 04:48:24 +0600

    data types

---
+++ ZTM/DataTypes.py
@@ -0,0 +1,30 @@
+# Fundamental Data Types
+# (int)
+# (float)
+# (bool)
+# (str)
+# (list)
+# (tuple)
+# (set)
+# (dict)
+
+# Classes -> custom type
+# (SuperCar)
+
+# Specialized Data Types
+# (modules)
+
+# None
+
+
+print(type(2+4))
+print(type(2-4))
+print(type(2*4))
+print(type(2/4))
+
+print(2 ** 3)
+print(5 // 4)
+print(5 % 4)
+
+print(9.9+1.1)
+print(type(9.9+1.1))
